c4aa87823cb850c57eb1b0200281e6ee83dfcbfb
gani-cybersec <128181057@sastra.ac.in>
2026-01-16 16:01:20 +0530

    Add basic password length check

---
+++ password_checker.py
@@ -0,0 +1,8 @@
+password = input("Enter your password: ")
+
+if len(password) < 8:
+    print("Password strength: Weak")
+else:
+    print("Password length is good")
+
+#Add basic password length check
