c9f42c66d0a4cbd00a48e7118a6acd609659de8c
gani-cybersec <128181057@sastra.ac.in>
2026-01-16 17:02:31 +0530

    Add special character check

--- password_checker.py
+++ password_checker.py
@@ -1,6 +1,8 @@
 password=input("Enter the password:")
 score=0
-if len(password) <= 8:  #checks the length of the password
+#checks the length of the password
+
+if len(password) <= 8:#checks the length of the password
     print("password should be greater than equal to 8 charectors")
 else:
     score+=1
@@ -15,6 +17,7 @@ if has_upper:
     score+=1
 else:
     print("Add at least one uppercase letter")
+#Number check    
 has_digit=False
 for char in password :
     if char.isdigit():
@@ -24,7 +27,20 @@ for char in password :
 if has_digit:
     score+=1
 else:
-    print("Add at least one number")    
+    print("Add at least one number")
+#special character
+special_chars="!@#$%^&*"
+has_special=False
+    
+for char in password :
+    if char in special_chars:
+        has_special=True
+        break
+
+if has_special:
+    score+=1
+else:
+    print("Add at least one special character(!@#$%^&*)")    
 print(f"password score:{score}")
 
 
